fd243bf81b3156ca73dbbe9a0dc8951c8e9461ac
stoskas <44064543+stoskas@users.noreply.github.com>
2020-04-13 20:06:21 +0300

    Update emdee_for_five.py

--- emdee_for_five.py
+++ emdee_for_five.py
@@ -5,19 +5,19 @@ import hashlib
 
 url = "The url is from a CTFs platform challenge"
 
-### Get request
+Get request
 r= requests.session()
 x = r.get('The url is from a CTFs platform challenge')
 x = re.search("<h3 align='center'>+.*?</h3>", x.text)
 
-### Remove unnecessary informations
+#Remove unnecessary informations, Pythex will help you extract some regular expressions!
 x = re.search(">.*<", x[0])
 x = re.search("\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w",x[0])
 
-### Encode our given hash!
+#Encode our given hash!
 x = hashlib.md5(x[0].encode('utf-8')).hexdigest()
 
-### Post request
+#Post request
 data={'hash': x}
 x = r.post(url = url, data = data)
 
