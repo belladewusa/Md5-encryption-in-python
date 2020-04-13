a907a95d67454720a176823dd4a524404513e761
stoskas <44064543+stoskas@users.noreply.github.com>
2020-04-13 19:35:07 +0300

    Add files via upload

---
+++ emdee_for_five.py
@@ -0,0 +1,26 @@
+import requests
+import re
+import hashlib
+
+
+url = "The url is from a Hackthebox challenge"
+
+### Get request
+r= requests.session()
+x = r.get('The url is from a Hackthebox challenge')
+x = re.search("<h3 align='center'>+.*?</h3>", x.text)
+
+### Remove unnecessary informations
+x = re.search(">.*<", x[0])
+x = re.search("\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w",x[0])
+
+### Encode our given hash!
+x = hashlib.md5(x[0].encode('utf-8')).hexdigest()
+
+### Post request
+data={'hash': x}
+x = r.post(url = url, data = data)
+
+print(x.text)
+
+
