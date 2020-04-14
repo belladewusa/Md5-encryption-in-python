ab95da665cf61133b730be49dced6afc91cc2fd2
stoskas <44064543+stoskas@users.noreply.github.com>
2020-04-14 18:15:25 +0300

    Update md5.py

--- md5.py
+++ md5.py
@@ -10,14 +10,17 @@ r= requests.session()
 x = r.get('The url is from a CTFs platform challenge')
 x = re.search("<h3 align='center'>+.*?</h3>", x.text)
 
-#Remove unnecessary informations, Pythex will help you extract some regular expressions!
+# Remove unnecessary informations, Pythex will help you extract some regular expressions!
 x = re.search(">.*<", x[0])
 x = re.search("\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w",x[0])
 
-#Encode our given hash!
+# Now we have only the password that we want to md5 encrypt it!
+
+
+# Encode our given hash!
 x = hashlib.md5(x[0].encode('utf-8')).hexdigest()
 
-#Post request
+# Post request
 data={'hash': x}
 x = r.post(url = url, data = data)
 
